2e15a1a28eb7a80787ff786c2e8490704f8c3575
김경묵 <apsode211@naver.com>
2022-12-27 14:27:51 +0900

    수정

--- company/main.js
+++ company/main.js
@@ -58,35 +58,35 @@ $(document).ready(function(){
 
 
 // 헤더
-var beforePosition = document.documentElement.scrollTop
+// var beforePosition = document.documentElement.scrollTop
 
 
-document.addEventListener('scroll', function () {
+// document.addEventListener('scroll', function () {
 
-    var afterPosition = document.documentElement.scrollTop;
-
-
-    // 스크롤 맨 위
-
-    if (window.scrollY != 0) {
-    }
+//     var afterPosition = document.documentElement.scrollTop;
 
 
+//     // 스크롤 맨 위
 
-    if (afterPosition > 50) {
+//     if (window.scrollY != 0) {
+//     }
 
-        if (beforePosition < afterPosition) {
-            // 스크롤 아래로
-            document.querySelector('header').style.display = 'none'
-        } else {
-            // 스크롤 위로 
-            document.querySelector('header').style.display = 'flex'
-            console.log(111);
-        }
 
-    } else {
-        // 평상 시
 
-    }
-    beforePosition = afterPosition;
-});
+//     if (afterPosition > 50) {
+
+//         if (beforePosition < afterPosition) {
+//             // 스크롤 아래로
+//             document.querySelector('header').style.display = 'none'
+//         } else {
+//             // 스크롤 위로 
+//             document.querySelector('header').style.display = 'flex'
+//             console.log(111);
+//         }
+
+//     } else {
+//         // 평상 시
+
+//     }
+//     beforePosition = afterPosition;
+// });
